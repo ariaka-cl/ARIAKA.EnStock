43fa36cc36be808895aceee040a70e90f0083eeb
btabilo <btabilosk8@gmail.com>
2017-04-08 02:27:26 -0400

    Mantenedor de Marcas
    Se agregan las definiciones par las marcas

--- ARIAKA.EnStock.Win/Controls/MarcaViewControl.vb
+++ ARIAKA.EnStock.Win/Controls/MarcaViewControl.vb
@@ -1,5 +1,34 @@
 ﻿Namespace Controls
     Public Class MarcaViewControl
+        Private Property _cliente As New Logica.Cliente
+
+        Public Sub MarcaViewControl_Load() Handles MyBase.Load
+            PopulateMarcaGrid()
+        End Sub
+
+        Public Sub PopulateMarcaGrid()
+            Dim listMarcaDto As List(Of Models.MarcaDTO) = _cliente.GetMarca()
+            If listMarcaDto IsNot Nothing Then
+                Me.MarcaControl.MarcaDTOBindingSource.DataSource = listMarcaDto
+                Me.MarcaControl.GridView1.RefreshEditor(True)
+            End If
+
+        End Sub
+        Private Sub SimpleButton_Guardar_Click(sender As Object, e As EventArgs) Handles SimpleButton_Guardar.Click
+            Dim marcaDto As New Models.MarcaDTO With {.Nombre = Me.TextEdit_Nombre.Text}
+            marcaDto = _cliente.GuardarMarcas(marcaDto)
+            Me.MarcaControl.MarcaDTOBindingSource.Add(marcaDto)
+            Me.MarcaControl.GridView1.RefreshEditor(True)
+        End Sub
+
+        Private Sub SimpleButton_Eliminar_Click(sender As Object, e As EventArgs) Handles SimpleButton_Eliminar.Click
+            Dim categoriaDto As Models.MarcaDTO = CType(MarcaControl.MarcaDTOBindingSource.Current, Models.MarcaDTO)
+
+            Dim id As Integer = categoriaDto.ID
+            If id <> 0 Then _cliente.EliminarMarca(id)
+            MarcaControl.GridView1.DeleteRow(MarcaControl.GridView1.FocusedRowHandle)
+            Me.MarcaControl.GridView1.RefreshEditor(True)
+        End Sub
 
     End Class
 End Namespace
---
+++ ARIAKA.EnStock.Win/Logica/Cliente.vb
@@ -0,0 +1,60 @@
+﻿Imports System.Windows.Forms
+Imports ARIAKA.EnStock.Data
+Namespace Logica
+    Public Class Cliente
+        Public Function GetMarca() As List(Of Models.MarcaDTO)
+            Dim db As New EnStockContext
+            Try
+                Dim listMarca As List(Of Marca) = db.Marca.ToList()
+                Dim listMarcaDto As New List(Of Models.MarcaDTO)
+                For Each marca As Marca In listMarca
+
+                    listMarcaDto.Add(New Models.MarcaDTO With {.ID = marca.ID,
+                                                                   .Nombre = marca.Nombre})
+                Next
+
+                Return listMarcaDto
+            Catch ex As Exception
+                MessageBox.Show(String.Format("Error : {0}", ex.Message), "Error Obtener Marcas", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return New List(Of Models.MarcaDTO)
+            Finally
+                db.Dispose()
+            End Try
+        End Function
+
+        Public Function GuardarMarcas(marcasDto As Models.MarcaDTO) As Models.MarcaDTO
+            Dim db As New EnStockContext
+            Try
+                Dim marca As New Marca With {.Nombre = marcasDto.Nombre}
+                db.Marca.Add(marca)
+                db.SaveChanges()
+                marcasDto.ID = marca.ID
+
+                Return marcasDto
+            Catch ex As Exception
+                MessageBox.Show(String.Format("Error : {0}", ex.Message), "Error Agregar Marca", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return New Models.MarcaDTO
+            Finally
+                db.Dispose()
+            End Try
+        End Function
+
+        Public Function EliminarMarca(id As Integer) As Boolean
+            Dim db As New EnStockContext
+            Try
+                Dim marca As Marca = db.Marca.Where(Function(m) m.ID = id).SingleOrDefault()
+                db.Marca.Remove(marca)
+                db.SaveChanges()
+                Return True
+
+            Catch ex As Exception
+                MessageBox.Show(String.Format("Error : {0}", ex.Message), "Error Eliminar Marca", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return False
+            Finally
+                db.Dispose()
+            End Try
+        End Function
+
+
+    End Class
+End Namespace
