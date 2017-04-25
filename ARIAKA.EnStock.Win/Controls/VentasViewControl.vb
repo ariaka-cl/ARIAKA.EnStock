8ec324cab903a07598c616c5754373383f562bbb
btabilo <btabilosk8@gmail.com>
2017-04-24 23:33:37 -0400

    Se eliminan las ventas

--- ARIAKA.EnStock.Win/Controls/VentasViewControl.vb
+++ ARIAKA.EnStock.Win/Controls/VentasViewControl.vb
@@ -13,5 +13,28 @@
             Next
             LabelControl_Total_Value.Text = total.ToString()
         End Sub
+
+        Private Sub SimpleButton_Delete_Click(sender As Object, e As EventArgs) Handles SimpleButton_Delete.Click
+            Dim ventaDTO As Models.VentasDTO = CType(TryCast(Me.VentasGridControl1.VentasDTOBindingSource, BindingSource).Current, Models.VentasDTO)
+            Dim totalVenta As Integer = 0
+            Dim total As Integer = 0
+            If ventaDTO IsNot Nothing AndAlso ventaDTO.ID <> 0 Then
+                Dim answer As DialogResult = MessageBox.Show("Desea eliminar venta seleccionada", "Eliminar Venta", MessageBoxButtons.YesNo, MessageBoxIcon.Information)
+                If answer = DialogResult.Yes Then
+                    totalVenta = CInt(ventaDTO.Total)
+                    If _cliente.EliminarVenta(ventaDTO.ID) Then
+                        MessageBox.Show("Se elimino venta correctamente", "Eliminar Venta", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                        Me.VentasGridControl1.GridView1.DeleteRow(Me.VentasGridControl1.GridView1.FocusedRowHandle)
+                        total = CInt(Me.LabelControl_Total_Value.Text)
+                        total = total - totalVenta
+                        Me.LabelControl_Total_Value.Text = total.ToString()
+                        Me.VentasGridControl1.GridView1.RefreshEditor(True)
+                    Else
+                        MessageBox.Show("Ocurrio un Error al eliminar venta", "Eliminar Venta", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    End If
+                End If
+                Return
+            End If
+        End Sub
     End Class
 End Namespace
--- ARIAKA.EnStock.Win/Logica/Cliente.vb
+++ ARIAKA.EnStock.Win/Logica/Cliente.vb
@@ -304,5 +304,31 @@ Namespace Logica
             End Try
         End Function
 
+        Public Function EliminarVenta(ventaID As Integer) As Boolean
+            Dim db As New EnStockContext
+            Try
+                Dim Listdetalle As List(Of DetalleVentas) = db.DetalleVentas.Where(Function(dv) CBool(dv.VentasID = ventaID)).ToList()
+                For Each detalle As DetalleVentas In Listdetalle
+                    Dim produID As Integer = CInt(detalle.ProductoID)
+                    db.DetalleVentas.Remove(detalle)
+                    If produID = 0 Then Return False
+                    Dim producto As Productos = db.Productos.Where(Function(p) CBool(p.ID = produID)).SingleOrDefault()
+                    If producto IsNot Nothing Then
+                        producto.Stock = producto.Stock + detalle.Cantidad
+                    End If
+                    db.SaveChanges()
+                Next
+                Dim venta As Ventas = db.Ventas.Where(Function(v) v.ID = ventaID).SingleOrDefault()
+                db.Ventas.Remove(venta)
+                db.SaveChanges()
+                Return True
+            Catch ex As Exception
+                MessageBox.Show(String.Format("Error : {0}", ex.Message), "Error elimnar venta", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return False
+            Finally
+                db.Dispose()
+            End Try
+        End Function
+
     End Class
 End Namespace
